7e290be1e99b2064b2d2a8314e94f071efbc33c2
Julieta Alcaraz Aballay <142432213+jul1alcaraz@users.noreply.github.com>
2025-11-30 17:30:38 -0300

    Update Social.jsx

--- client/src/components/Social.jsx
+++ client/src/components/Social.jsx
@@ -5,50 +5,66 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import WorkIcon from "@mui/icons-material/Work";
 import { Tooltip, Link } from '@mui/material';
 
 const Social = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <Tooltip title="Preguntas frecuentes">
+        <Link href="/FaqPage" target="_blank" sx={{ my: 2, mr: 3, color: 'white' }}>
+          <QuestionMarkIcon />
+        </Link>
+      </Tooltip>
 
-            <Tooltip title="Preguntas frecuentes">
-                <Link href='/FaqPage' target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <QuestionMarkIcon></QuestionMarkIcon>
-                </Link>
-            </Tooltip>
+      <Tooltip title="Portafolio">
+        <Link
+          href="https://jul1alcaraz.netlify.app/"
+          target="_blank"
+          sx={{ my: 2, mr: 3 }}
+        >
+          <WorkIcon />
+        </Link>
+      </Tooltip>
 
-            <Tooltip title="Facebook">
-                <Link href="https://www.facebook.com/?locale=es_LA" target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <FacebookIcon></FacebookIcon>
-                </Link>
-            </Tooltip>
+      <Tooltip title="GitHub">
+        <Link
+          href="https://github.com/jul1alcaraz"
+          target="_blank"
+          sx={{ my: 2, mr: 3 }}
+        >
+          <GitHubIcon />
+        </Link>
+      </Tooltip>
 
-            <Tooltip title="Twitter">
-                <Link href="https://twitter.com/?lang=es" target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <TwitterIcon></TwitterIcon>
-                </Link>
-            </Tooltip>
+      <Tooltip title="LinkedIn">
+        <Link
+          href="https://www.linkedin.com/in/jul1alcaraz/"
+          target="_blank"
+          sx={{ my: 2, mr: 3 }}
+        >
+          <LinkedInIcon />
+        </Link>
+      </Tooltip>
 
-            <Tooltip title="Instagram">
-                <Link href="https://www.instagram.com/" target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <InstagramIcon></InstagramIcon>
-                </Link>
-            </Tooltip>
+      <Tooltip title="Instagram">
+        <Link
+          href="https://www.instagram.com/"
+          target="_blank"
+          sx={{ my: 2, mr: 3, color: 'white' }}
+        >
+          <InstagramIcon />
+        </Link>
+      </Tooltip>
 
-            <Tooltip title="LinkedIn">
-                <Link href="https://www.linkedin.com/in/jul1alcaraz/" target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <LinkedInIcon></LinkedInIcon>
-                </Link>
-            </Tooltip>
-
-            <Tooltip title="Comunicate con nosotros">
-                <Link href="/contact" target="_blank" sx={{ my: 2, marginRight: 3, color: 'white' }}>
-                    <ContactMailIcon></ContactMailIcon>
-                </Link>
-            </Tooltip>
-
-        </div>
-    );
+      <Tooltip title="Comunicate con nosotros">
+        <Link href="/contact" target="_blank" sx={{ my: 2, mr: 3, color: 'white' }}>
+          <ContactMailIcon />
+        </Link>
+      </Tooltip>
+    </div>
+  );
 };
 
 export default Social;
